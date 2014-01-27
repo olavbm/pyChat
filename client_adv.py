edc9f1dafed5448626a63457c0fa1a3ca3b4a1f4
shorm <olavmarkussen@hotmail.no>
2014-01-27 23:54:07 +0100

    Trying to make some changes, stil getting nowhere with select.select[SOCKET]..

--- client_adv.py
+++ client_adv.py
@@ -1,6 +1,5 @@
 # -*- coding: utf-8 -*-
-import socket, json, select, sys
-#Hello
+import sys, socket, json, select
 class Client(object):
 
 	def __init__(self):
@@ -11,6 +10,7 @@ class Client(object):
 			return select.select([sys.stdin], [], [], 0) ==	([sys.stdin], [], [])
 		# Start tilkoblingen
 		self.connection.connect((host, port))
+		#self.connection.listen(5)
 		# Be brukeren om å skrive inn brukernavn
 		nick = raw_input('name: ')
 		data = {'nick':nick, 'message':" just connected"}
@@ -22,15 +22,17 @@ class Client(object):
 				line = sys.stdin.readline()
 				if len(line) < 1: continue
 				if line:
-					data = {'nick':nick, 'message':line}
+					data = {'nick':nick, 'message':line[:len(line)-1]}
 					data = json.dumps(data)
 					self.send(data)
 					print "Sent", data,"successfully"
-	#		while connIn in select.select([self.connection.recv(1024)], [], [], 0):
-			while self.connection.recv(1024):
-				print "Got here", connIn
-				recievedData = connIn
-				print recievedData
+					print connIn
+					print "SelfCon:", self.connection
+#			while connIn in select.select([self.connection.recv(1024)], [], [], 0):
+			while connIn in select.select([self.connection], [],[],0)[0]:
+				print "GOT FUCKINGSHERE!!!!!"
+				data = json.loads(connIn)
+				print "Data:",data
 			connIn += 1
 	def send(self, data):
 		self.connection.send(data)
